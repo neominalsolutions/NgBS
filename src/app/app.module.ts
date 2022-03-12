f72a4bd213792dc259e05bdf764724cd5808b407
mert.alptekin <mert.alptekin@neominal.com>
2022-03-12 10:23:05 +0300

    AngularLessonOne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,22 +8,20 @@ import { AppComponent } from './app.component';
 import { BsModule } from './components/bs.module';
 import { ComponentsModule } from './components/components.module';
 import { HomeComponent } from './pages/home/home.component';
+import { TodosComponent } from './pages/todos/todos.component';
 // ngx-dropdown module import ettik.
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent, TodosComponent],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ComponentsModule,
-    BsModule // Pages sayfaları için bootrap kullanırız
+    BsModule, // Pages sayfaları için bootrap kullanırız
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
